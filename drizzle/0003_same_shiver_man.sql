b3b51094bcaf3864406db5a7fb18bb7ab7cb1d75
ANIRUDDH-001 <aniruddhvijay2k7@gmail.com>
2026-03-12 22:09:24 +0530

    chore: remove drizzle from tracking and add to .gitignore

--- drizzle/0003_same_shiver_man.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "students" ALTER COLUMN "sap_id" SET DATA TYPE varchar(9);--> statement-breakpoint
-ALTER TABLE "students" ALTER COLUMN "roll_no" SET DATA TYPE varchar(11);
--- drizzle/0007_add_shortlisted.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE rankings ADD COLUMN IF NOT EXISTS shortlisted boolean;
-CREATE INDEX IF NOT EXISTS idx_rankings_drive_shortlisted
-  ON rankings(drive_id, shortlisted) WHERE shortlisted = true;
